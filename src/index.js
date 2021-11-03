6d45047d04476eeec152e9a9c1e81afc8ea639f1
rolandm99 <manfulmweze99@gmail.com>
2021-11-03 20:33:23 +0100

    Add list structure for the to do app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
-}
-document.body.appendChild(component());
+const myTasks = [
+  {
+    description: 'Complete my TO-DO-List project',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'cook my food for tonight',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Read OOP in Java for preparation of tuesday Quiz',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Relax with a Netflix movie THE HATE WE GIVE',
+    completed: false,
+    index: 3,
+  },
+];
+
+myTasks.forEach((element) => {
+  const myTodoView = document.createElement('todo-view');
+  myTodoView.innerHTML = `<li>
+  <span class="material-icons show-default">check_box_outline_blank</span>
+  <p contenteditable="true">${element.description}</p>
+  <span class="material-icons more-info">more_vert</span>
+</li>`;
+document.querySelector('body > main > section > div.input-container > div > ul').appendChild(myTodoView);
+});
